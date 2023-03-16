4a0c6ac6d6dd0efcb6f056d070b66bb76b2e3e0a
Yurii Tereschenko <yu0303@yandex.ru>
2023-03-16 12:46:15 +0500

    corrections have been made in task1

--- task1.go
+++ task1.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 )
 
-func claculating(x int16, y uint8, z float32) float32 {
-
-	return 2*float32(x) + float32(y)*float32(y) - 3/z
+func claculating(x int16, y uint8, z float32, F func(float32, float32, float32) float32) float32 {
+	return F(float32(x), float32(y), z)
 }
 func main() {
 	var (
@@ -14,5 +13,5 @@ func main() {
 		y uint8   = 5
 		z float32 = 1.0
 	)
-	fmt.Println(claculating(x, y, z))
+	fmt.Println(claculating(x, y, z, func(a, b, c float32) float32 { return 2*a + b*b - 3/c }))
 }
